e6b608c099e313071fdf27c35b5a8e638d441551
i-vishi <rr.vishalgaur@gmail.com>
2020-12-31 14:59:08 +0530

    added navigation

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,50 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app>
-      <!--  -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h4">
+            TOPICS
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6 font-weight-light">{{
+              item.title
+            }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title class="text-h4 font-weight-light"
+        >Data Structures and Algorithms</v-toolbar-title
+      >
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  data: () => ({ drawer: null })
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Hello World" },
+      { title: "Sorting" },
+      { title: "Searching" }
+    ]
+  })
 };
 </script>
