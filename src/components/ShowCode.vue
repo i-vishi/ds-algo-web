0bf531c8e5a51bbbc040e39b5cde6f5ef29ad554
i-vishi <rr.vishalgaur@gmail.com>
2021-01-03 17:10:31 +0530

    added compiler

--- src/components/ShowCode.vue
+++ src/components/ShowCode.vue
@@ -13,33 +13,55 @@
     ></v-skeleton-loader>
     <v-tabs-items v-model="tab">
       <v-tab-item v-for="item in codes" :key="item.tab">
-        <v-card flat class="text-h7 ma-6" color="#040404">
-          <highlight-code :lang="item.langname">{{
-            item.content
-          }}</highlight-code>
+        <v-card flat>
+          <v-card-text class="text-body-1">
+            <highlight-code :lang="item.langname">{{
+              item.content
+            }}</highlight-code>
+          </v-card-text>
+          <v-divider />
+          <v-card-actions>
+            <v-btn
+              color="green"
+              text
+              @click="compileCode(item.langname, item.content)"
+            >
+              Run
+            </v-btn>
+            <v-spacer />
+            <v-btn
+              color="red"
+              text
+              @click="compileShow = false"
+              v-show="compileShow"
+            >
+              Close
+            </v-btn>
+          </v-card-actions>
         </v-card>
       </v-tab-item>
     </v-tabs-items>
-    <v-divider />
-    <v-card-actions>
-      <v-btn color="green" text @click="compileCode">
-        Run
-      </v-btn>
-      <v-spacer />
-      <v-btn color="red" text @click="compileShow = false" v-show="compileShow">
-        Close
-      </v-btn>
-    </v-card-actions>
+
     <v-expand-transition>
       <div v-show="compileShow">
         <v-divider></v-divider>
 
         <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he
-          could deliver. You won't have time for sleeping, soldier, not with all
-          the bed making you'll be doing. Then we'll go with that data file!
-          Hey, you add a one and two zeros to that or we walk! You're going to
-          do his laundry? I've got to find a way to escape.
+          <div v-show="compileStatus !== 'none'" class="text-overline">
+            <span class="green--text">{{ statusMessage[compileStatus] }}</span>
+          </div>
+          <div v-show="compileStatus === 'completed'" class="text-overline">
+            ---------
+          </div>
+          <div v-show="compileStatus === 'completed'" class="text-overline">
+            output
+          </div>
+          <div v-show="compileStatus === 'completed'" class="text-overline">
+            ---------
+          </div>
+          <div v-show="compileStatus === 'completed'" class="text-body-2 my-3">
+            {{ compileResult.stdout }}
+          </div>
         </v-card-text>
       </div>
     </v-expand-transition>
@@ -47,17 +69,51 @@
 </template>
 
 <script>
+const axios = require("axios");
 export default {
   name: "ShowCode",
   props: ["codetitle", "codes"],
   data: () => ({
     tab: null,
     compileShow: false,
-    compileResult: ""
+    compileResult: {},
+    compileStatus: "none",
+    statusMessage: {
+      completed: "Successfully Compiled",
+      running: "Compiling Code...",
+      error: "Error compiling code..."
+    }
   }),
   methods: {
-    compileCode() {
+    async compileCode(langname, srcCode) {
       this.compileShow = true;
+      let submitId = "";
+      await axios
+        .post("http://api.paiza.io:80/runners/create", {
+          source_code: srcCode,
+          language: langname,
+          api_key: "guest"
+        })
+        .then(res => {
+          submitId = res.data.id;
+          this.compileStatus = res.data.status;
+        });
+      while (this.compileStatus !== "completed") {
+        await axios
+          .get("http://api.paiza.io:80/runners/get_status", {
+            params: { id: submitId, api_key: "guest" }
+          })
+          .then(res => {
+            this.compileStatus = res.data.status;
+          });
+      }
+      await axios
+        .get("http://api.paiza.io:80/runners/get_details", {
+          params: { id: submitId, api_key: "guest" }
+        })
+        .then(res => {
+          this.compileResult = res.data;
+        });
     }
   }
 };
